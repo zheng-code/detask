d2ed78218366d6bb863e13bd9f81e48269d77086
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2019-06-26 23:14:55 +0800

    Add an extremely simple unit test (#12)
    * Setup dependencies and add basic launch test
    
    * Remove extra empty line

--- app/src/androidTest/java/com/zhengcode/detask/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/zhengcode/detask/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.zhengcode.detask
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,21 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    // This is required to fire off the Activity that you want to test
+    @Rule
+    @JvmField
+    val rule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.zhengcode.detask", appContext.packageName)
+    }
+
+    @Test
+    fun testLaunch() {
+        //
+        onView(withId(R.id.message)).check(matches(withText("detask")))
     }
 }
