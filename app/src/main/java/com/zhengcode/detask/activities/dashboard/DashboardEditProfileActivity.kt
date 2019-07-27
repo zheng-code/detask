ff38f218fe23c4f22ba5e2cf61569ea3d30c7217
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2019-07-27 18:47:27 +0800

    Redesign UI for edit profile page (#54)
    * Minor tweak to edit profile UI
    
    * Hide phone numbers
    
    * Add new edit profile page
    
    * Add functionalities for navbar buttons in EditProfile

--- app/src/main/java/com/zhengcode/detask/activities/dashboard/DashboardEditProfileActivity.kt
+++ app/src/main/java/com/zhengcode/detask/activities/dashboard/DashboardEditProfileActivity.kt
@@ -1,12 +1,16 @@
 package com.zhengcode.detask.activities.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.database.FirebaseDatabase
 import com.zhengcode.detask.R
+import com.zhengcode.detask.activities.taskmanager.TaskManagerActivity
+import com.zhengcode.detask.activities.tasks.TasksActivity
 import com.zhengcode.detask.utils.Helpers
 import com.zhengcode.detask.utils.showToast
 import kotlinx.android.synthetic.main.activity_dashboard_edit_profile.*
@@ -14,16 +18,36 @@ import kotlinx.android.synthetic.main.activity_dashboard_edit_profile.*
 class DashboardEditProfileActivity : AppCompatActivity() {
     private val currentUser = FirebaseAuth.getInstance().currentUser
 
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_dashboard -> {
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.navigation_tasks -> {
+                val intent = Intent(this, TasksActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.navigation_task_manager -> {
+                val intent = Intent(this, TaskManagerActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_edit_profile)
+        setContentView(R.layout.activity_dashboard_edit_profile_2)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
         currentUser?.let { user ->
             edit_text_name.setText(user.displayName)
 
             text_email.text = user.email
 
-            text_phone.text = if (user.phoneNumber.isNullOrEmpty()) "Add Number" else user.phoneNumber
+//            text_phone.text = if (user.phoneNumber.isNullOrEmpty()) "Add Number" else user.phoneNumber
 
             if (user.isEmailVerified) {
                 text_not_verified.visibility = View.INVISIBLE
